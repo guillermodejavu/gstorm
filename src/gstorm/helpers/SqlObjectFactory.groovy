0d31c68ad58b5d9aac427bdcfed025cd8f2724a0
Guillermo <guillermo.dejavu@gmail.com>
2014-04-11 12:45:30 +0200

    Update SqlObjectFactory.groovy

--- src/gstorm/helpers/SqlObjectFactory.groovy
+++ src/gstorm/helpers/SqlObjectFactory.groovy
@@ -14,4 +14,8 @@ class SqlObjectFactory {
     static Sql fileDB(String path) {
         Sql.newInstance('jdbc:hsqldb:file:' + path, 'sa', '', 'org.hsqldb.jdbcDriver')
     }
+    
+    static Sql MysqlDB(String host, String schema, String user, String password) {
+        Sql.newInstance('jdbc:mysql://' + host + '/' + schema, username, password, 'com.mysql.jdbc.Driver')
+    }
 }
